18cb92ea35ae37dc19a5e3bab9a4063c98c2446c
bfay <584633512@qq.com>
2019-12-16 17:04:49 +0800

    demo

--- src/main/java/cn/bfay/DemoApplication.java
+++ src/main/java/cn/bfay/DemoApplication.java
@@ -2,12 +2,14 @@ package cn.bfay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+        System.out.println("-------------" + applicationContext.getBeanDefinitionCount());
     }
 
 }
---
+++ src/main/java/cn/bfay/controller/DemoController.java
@@ -0,0 +1,13 @@
+package cn.bfay.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * DemoController.
+ *
+ * @author wangjiannan
+ * @since 2019/12/16
+ */
+@Controller("/demo")
+public class DemoController {
+}
